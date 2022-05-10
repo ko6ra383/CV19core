7dae16f9435b4a4dd34502ac0fa0a2a39f1b7a91
Kobra Matt <matveiamelhcenko@mail.ru>
2022-05-10 22:42:57 +0700

    Тестовая предметная область

---
+++ CV19/Models/Decanat/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CV19.Models.Decanat
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public DateTime Birtday { get; set; }
+        public double Rating { get; set; }
+    }
+
+    public class Group
+    {
+        public string Name { get; set; }
+        public ICollection<Student> Students { get; set; }
+}
