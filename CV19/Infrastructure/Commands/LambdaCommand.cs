8af8d0bebb45c6302dddf9e1c4f0aec0f7297b91
Kobra Matt <matveiamelhcenko@mail.ru>
2022-05-10 21:50:38 +0700

    Добавление лямбда комманд

---
+++ CV19/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,20 @@
+﻿using CV19.Infrastructure.Commands.Base;
+using System;
+
+namespace CV19.Infrastructure.Commands
+{
+    public class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        public override bool CanExecute(object? parameter) => _CanExecute?.Invoke(parameter) ?? true;
+        public override void Execute(object? parameter) => _Execute(parameter);
+    }
+}
