8b000fef63999e6b56fe95e5b26737e18dbf7cc3
Kobra Matt <matveiamelhcenko@mail.ru>
2022-05-10 21:32:06 +0700

    Модель представления главного окна

---
+++ CV19/ViewsModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CV19.ViewsModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.ViewsModels
+{
+    internal class MainWindowViewModel:BaseViewModel
+    {
+        #region Свойства
+
+        #region Заголовок окна
+        private string _Title;
+
+        public string Title
+        {
+            get { return _Title; }
+            set { Set(ref _Title, value); }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Команды
+
+        #endregion
+        public MainWindowViewModel()
+        {
+            #region Команды
+
+            #endregion
+        }
+    }
+}
