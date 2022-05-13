ef86139021686a0162b271a08e24bd010ced7dd4
Kobra Matt <matveiamelhcenko@mail.ru>
2022-05-14 03:03:52 +0700

    Фильтрация групп в разметке

--- CV19/Views/Windows/MainWindow.xaml.cs
+++ CV19/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using CV19.Models.Decanat;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace CV19
 {
@@ -7,6 +10,25 @@ namespace CV19
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GroupsCollectionFilter(object sender, FilterEventArgs e)
+        {
+            if (!(e.Item is Group group)) return;
+            if (group.Name is null) return;
+
+            var filterText = GroupNameFilterText.Text;
+            if (filterText.Length == 0) return;
+            if (group.Name.Contains(filterText, System.StringComparison.OrdinalIgnoreCase)) return;
+
+            e.Accepted = false;
+        }
+
+        private void OnGroupFilterChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            var textbox = (TextBox)sender; 
+            var collection = (CollectionViewSource)textbox.FindResource("GroupsCollection");
+            collection.View.Refresh();
         }
     }
 }
