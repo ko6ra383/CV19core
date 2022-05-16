73675df6cdd7ac2fef7cbe98068e99d82c5e535e
Kobra Matt <matveiamelhcenko@mail.ru>
2022-05-16 16:48:52 +0700

    Структура классов для описание стран

---
+++ CV19/Models/ConfirmedCount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.Models
+{
+    public struct ConfirmedCount
+    {
+        public DateTime Date { get; set; }
+        public int Count { get; set; }
+
+    }
+}
