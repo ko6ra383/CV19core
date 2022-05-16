bed02d885a3a529434f31b3a2cb844945fd1fafe
Kobra Matt <matveiamelhcenko@mail.ru>
2022-05-17 01:12:54 +0700

    Базовая логика модели-представления статистики

--- CV19/ViewsModels/CountriesStatisticViewModel.cs
+++ CV19/ViewsModels/CountriesStatisticViewModel.cs
@@ -1,10 +1,13 @@
-﻿using CV19.Services;
+﻿using CV19.Infrastructure.Commands;
+using CV19.Models;
+using CV19.Services;
 using CV19.ViewsModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CV19.ViewsModels
 {
@@ -13,10 +16,37 @@ namespace CV19.ViewsModels
         private DataServices _DataService;
         public MainWindowViewModel MainModel { get; }
 
+        #region Countries
+        /// <summary>
+        ///             Статистика по странам
+        /// </summary>
+        private IEnumerable<CountryInfo> _Countries;
+
+        public IEnumerable<CountryInfo> Countries
+        {
+            get => _Countries; 
+            private set => Set(ref _Countries, value);
+        }
+
+        #endregion
+
+        #region Команды
+        public ICommand RefreshDataCommand { get; }
+        private void OnRefreshDataCommandExecuted(object p)
+        {
+            Countries = _DataService.GetData();
+        }
+        
+        #endregion
 
         public CountriesStatisticViewModel(MainWindowViewModel MainModel)
         {
             this.MainModel = MainModel;
+            _DataService = new DataServices();
+
+            #region Команды
+            RefreshDataCommand = new LambdaCommand(OnRefreshDataCommandExecuted);
+            #endregion
         }
 
     }
