7816c1d26cd2cfa087080b8d085a7b341c92bb07
Kobra Matt <matveiamelhcenko@mail.ru>
2022-05-16 21:24:24 +0700

    Создаем дочернюю модель-представления

---
+++ CV19/ViewsModels/CountriesStatisticViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CV19.Services;
+using CV19.ViewsModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.ViewsModels
+{
+    public class CountriesStatisticViewModel : BaseViewModel
+    {
+        private DataServices _DataService;
+        public MainWindowViewModel MainModel { get; }
+
+
+        public CountriesStatisticViewModel(MainWindowViewModel MainModel)
+        {
+            this.MainModel = MainModel;
+        }
+
+    }
+}
