83feb4bbe3c0dc9612757a31f1d5ac65c99e4be0
Kobra Matt <matveiamelhcenko@mail.ru>
2022-05-20 03:54:53 +0700

    Расширение разметки

---
+++ CV19/Infrastructure/Common/StringToIntArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace CV19.Infrastructure.Common
+{
+    [MarkupExtensionReturnType(typeof(int[]))]
+    public class StringToIntArray : MarkupExtension
+    {
+        [ConstructorArgument("Str")]
+        public string Str { get; set; }
+        public char Separator { get; set; } = ';';
+        public StringToIntArray(){}
+        public StringToIntArray(string str)
+        {
+            Str = str;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) =>
+            Str.Split(new[] {Separator}, StringSplitOptions.RemoveEmptyEntries)
+                .DefaultIfEmpty()
+                .Select(int.Parse)
+                .ToArray();
+        
+    }
+}
