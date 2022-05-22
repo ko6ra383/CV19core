06f1c8a027832bf332b1603d5ec571e4d63bc1a1
Kobra Matt <matveiamelhcenko@mail.ru>
2022-05-22 17:44:41 +0700

    Инфраструктура для создания хоста приложения

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using CV19.Services;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -6,6 +8,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using CV19.ViewsModels;
 
 namespace CV19
 {
@@ -15,12 +18,20 @@ namespace CV19
     public partial class App : Application
     {
         public static bool IsDesingnMode { get; private set; } = true;
+
+      
         protected override void OnStartup(StartupEventArgs e)
         {
             IsDesingnMode = false;
             base.OnStartup(e);
 
 
+        }
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<DataServices>();
+            services.AddSingleton<CountriesStatisticViewModel>();
         }
     }
 }
--- CV19/Program.cs
+++ CV19/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +16,22 @@ namespace CV19
             var app = new App();
             app.InitializeComponent();
             app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host_builder = Host.CreateDefaultBuilder(args);
+
+            host_builder.UseContentRoot(Environment.CurrentDirectory);
+            host_builder.ConfigureAppConfiguration((host, cfg) =>
+            {
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                cfg.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            });
+
+            host_builder.ConfigureServices(App.ConfigureServices);
+
+            return host_builder;
         }
     }
 }
