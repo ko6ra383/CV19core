49245a96c705fa8ece9138030f55521f6a58592a
Kobra Matt <matveiamelhcenko@mail.ru>
2022-05-22 14:10:19 +0700

    Собственная точка входа в приложение - класс Program

---
+++ CV19/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
